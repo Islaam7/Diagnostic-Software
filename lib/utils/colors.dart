f3e49b5b0a6143950b4b1045b866ff6ec867059a
islaam7 <bgbos7077@gmail.com>
2025-02-16 16:24:29 +0200

    Add centralized color management in colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primary = Color(0xFF5800FF);
+  static const Color secondry = Color(0xFF222831);
+  static const Color background = Color(0xFFF7F7F7);
+  static const Color textcolors = Color(0xFF000000);
+}
