0497e3c997543cb04c3a9eb210891943f072685b
islaam7 <bgbos7077@gmail.com>
2025-02-19 00:26:42 +0200

    Deploy Flutter Web to GitHub Pages

--- lib/utils/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class AppColors{
-  static const Color primary = Color(0xFF5800FF);
-  static const Color secondry = Color(0xFF222831);
-  static const Color background = Color(0xFFF7F7F7);
-  static const Color black = Color(0xFF000000);
-  static const Color stroke = Color(0xFFAEAEB2);
-}
